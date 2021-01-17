2f31c243dc433295c968acdda4772ba3649469ea
Mishkovec <neilsa@mail.ru>
2021-01-17 18:44:52 +0300

    init node courses

--- index.js
+++ index.js
@@ -1,89 +1,30 @@
 const express = require('express')
-const booksRouter = express.Router()
+const path = require('path')
+const ehbs = require('express-handlebars')
 
 const app = express()
 
-// app.set('view engine', 'pug')
-// app.set('view engine', 'ejs')
+const homeRoute = require('./routes/home')
+const addRoute = require('./routes/add')
+const courseRoute = require('./routes/courses')
+
+const hbs = ehbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
-app.set('views', './views')
+app.set('views', 'views')
 
-const products = ['Apple', 'Banana', 'Grape']
+app.use(express.static('public'))
+app.use('/', homeRoute)
+app.use('/add', addRoute)
+app.use('/courses', courseRoute)
 
-app.use((req, res, next) => {
-    console.log("Date", new Date(), 'Method', req.method, 'URL', req.originalUrl, 'Ip', req.ip)
-    next()
-})
 
-app.use('/static', express.static(__dirname + '/public'))
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res, next) => {
-    res.send('EX Work')
-})
-
-app.get('/products', (req, res, next) => {
-    // res.send(products)
-    console.log('Page', req.query.page)
-    res.json({ products })
-})
-
-app.get('/products/:id', (req, res, next) => {
-    if (products[req.params.id]) {
-        res.send(products[req.params.id])
-    } else {
-        res.status(404).send('Product not found')
-    }
-})
-
-booksRouter.get('/', (req, res) => {
-    res.send('books')
-})
-
-booksRouter.get('/about', (req, res) => {
-    res.send('about books')
-})
-
-app.get('/blog', (req, res) => {
-    res.redirect('/')
-})
-
-app.get('/downloadBoks', (req, res) => {
-    res.download('./public/books.html', 'anyFileName', (err) => {
-        console.log('file sent')
-    })
-})
-
-app.get('/pug', (req, res, next) => {
-    res.render('main', {
-        title: 'Products',
-        message: 'Products List',
-        products
-    })
-})
-
-app.get('/ejs', (req, res, next) => {
-    res.render('main', {
-        title: 'Products',
-        message: 'Products List',
-        products
-    })
-})
-
-app.get('/hbs', (req, res, next) => {
-    res.render('main', {
-        title: 'Products',
-        message: 'Products List',
-        products
-    })
-})
-
-app.use('/books', booksRouter)
-
-app.use((err, req, res, next) => {
-    console.log(err.status)
-    res.status(500).send(err.stack)
-})
-
-app.listen(5000, () => {
-    console.log('start', new Date())
+app.listen(PORT, () => {
+    console.log('Running app')
 })
---
+++ routes/home.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router.get('/', (req, res, next) => {
+    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index', {
+        title: 'Home',
+        isHome: true
+    })
+})
+
+
+module.exports = router
